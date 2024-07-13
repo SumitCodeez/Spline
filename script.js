82c74467fe0af3518e2c0b4f1485c3e02debf68f
user.name like "Sumeet Dipanee <user. email like "sumitdeepani00@gmail.com>
2024-07-14 02:05:18 +0530

    v

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function startSplineAnimation() {
       defaults: { duration: 3.2, ease: "expo.inOut" },
     });
 
-    tlspline.to(position, { y: 60 }).to(rotation, { y: 0.24 }, 0);
+    tlspline.to(position, { y: 70 }).to(rotation, { y: 0.24 }, 0);
   };
 
   gsap.set(canvas, { autoAlpha: 0 });
