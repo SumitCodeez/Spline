13a1abd7fd0f4dea9c737434b0e70f14658e45ab
user.name like "Sumeet Dipanee <user. email like "sumitdeepani00@gmail.com>
2024-07-14 01:30:38 +0530

    c

--- script.js
+++ script.js
@@ -165,19 +165,6 @@ function marqueeEffect() {
     currentScroll = this.window.pageYOffset;
   });
 }
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.navbar a').forEach(function(link) {
-    link.addEventListener('mouseenter', function() {
-      console.log("mouseenter");
-      this.classList.add('hover-effect');
-    });
-
-    link.addEventListener('mouseleave', function() {
-      this.classList.remove('hover-effect');
-      console.log("mouseleave");
-    });
-  });
-});
 
 
 window.addEventListener("DOMContentLoaded", function () {
