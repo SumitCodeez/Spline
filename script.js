bba0835c247b1acbf0c8843c57ef6815e6af308f
user.name like "Sumeet Dipanee <user. email like "sumitdeepani00@gmail.com>
2024-07-14 20:43:41 +0530

    k

--- script.js
+++ script.js
@@ -181,12 +181,12 @@ function parallaxEffect() {
   ScrollTrigger.create({
     trigger: ".wrapper",
     start: "top top",
-    end: "+=120vh",
+    end: "+=150vh",
     scrub: true,
     pin: true,
     onUpdate: (self) => {
       gsap.to(".wrapper", {
-        x: `${-350 * self.progress}vw`,
+        x: `${-250 * self.progress}vw`,
         duration: 0.5,
         ease: "power3.inOut",
       });
